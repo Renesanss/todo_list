7613a7442565830627b7f34b9176866b0fee1497
Renata Pientkowska <pientkowska.renata@gmail.com>
2022-11-24 19:27:42 +0100

    bindEvents moved to a separate function

--- js/script.js
+++ js/script.js
@@ -24,12 +24,37 @@
     render();
     }
 
+    const toggleTaskDone = (index) => {
+        tasks[index].done = !tasks[index].done;
+        render();
+    }
+
+const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+removeButton.addEventListener("click", () => {
+removeTask(index);
+});
+        });
+   
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+toggleDoneButton.addEventListener("click", () => {
+toggleTaskDone(index);
+});
+        });
+}
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
              <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+             <button class="js-done">zrobione</button>
              <button class="js-remove">usuń</button>
                 ${task.content}
             </li>
@@ -37,16 +62,10 @@
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-removeButton.addEventListener("click", () => {
-removeTask(index);
-});
-        })
+        bindEvents();
     };
-
 
 const onFormSubmit = (event) => {
     event.preventDefault();
